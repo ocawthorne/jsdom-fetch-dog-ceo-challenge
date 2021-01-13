7c9f1525c5dc8b941769dd4aa5230caaae4b989a
ocawthorne <ocawthorne@gmail.com>
2021-01-13 18:58:08 +0000

    fetch-dog

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+function fetchImages() {
+   return fetch(imgUrl)
+      .then(func => func.json())
+      .then(json => renderImages(json))
+}
+
+function renderImages(json) {
+   const image = document.querySelector('#dog-image-container')
+   json.forEach(image => {
+      debugger
+   })
+}
+document.addEventListener('DOMContentLoaded', fetchImages())
