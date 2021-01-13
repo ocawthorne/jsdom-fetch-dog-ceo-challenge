967b91e599129f42c883d4f2994d325e25dccf3e
ocawthorne <ocawthorne@gmail.com>
2021-01-13 19:41:25 +0000

    dog-ceo-challenge

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 console.log('%c HI', 'color: firebrick')
 
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+const breedUrl = "https://dog.ceo/api/breeds/list/all"
 
 function fetchImages() {
-   return fetch(imgUrl)
+   const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+   fetch(imgUrl)
       .then(func => func.json())
-      .then(json => renderImages(json))
+      .then(results => {
+         results.message.forEach(image => addImage(image))
+      })
 }
 
-function renderImages(json) {
-   const image = document.querySelector('#dog-image-container')
-   json.forEach(image => {
-      debugger
-   })
+function addImage(url) {
+   let container = document.querySelector('#dog-image-container')
+   let newEl = document.createElement('img')
+   newEl.src = url
+   container.appendChild(newEl)
 }
-document.addEventListener('DOMContentLoaded', fetchImages())
+
+function fetchBreeds() {
+   const breedUrl = "https://dog.ceo/api/breeds/list/all"
+   fetch(breedUrl)
+      .then(func => func.json())
+      .then(results => {
+         Object.keys(results.message).forEach(res => addBreed(res))
+      })
+}
+
+function addBreed(breed) {
+   let urlUL = document.querySelector('ul')
+   let newLI = document.createElement('li')
+   newLI.innerText = breed
+   urlUL.appendChild(newLI)
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+   fetchImages()
+   fetchBreeds()
+})
